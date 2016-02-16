8a858c0a5c55e5c495fcfccf6cd2290dfabddfe2
Brian Greene <bgreene623@gmail.com>
2016-02-16 15:21:33 -0500

    Used gulp to minify css

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+// Include gulp
+var gulp = require('gulp');
+
+var uglify = require('gulp-uglify');
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+// Default Task
+gulp.task('default', ['scripts']);
